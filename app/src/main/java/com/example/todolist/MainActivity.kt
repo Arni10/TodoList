fd0965f546e59ea00c5fb0c27476c4b2ed0f7472
Arni10 <arnycadabra@gmail.com>
2021-05-04 14:43:56 +0300

    This is our fourth commit

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         println("This is contained in my second commit")
+        blabla
 
         todoAdapter = TodoAdapter(mutableListOf())
 
