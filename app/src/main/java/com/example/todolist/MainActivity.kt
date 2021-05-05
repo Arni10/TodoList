dab1ce69d460f667ae9266201592fad81f5e8e8f
Arni10 <arnycadabra@gmail.com>
2021-05-05 12:53:19 +0300

    Change 1 commit

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -17,7 +17,7 @@ class MainActivity : AppCompatActivity() {
 
         println("Local change")
 
-        println("Experimental")
+        println("Change 1")
 
         todoAdapter = TodoAdapter(mutableListOf())
 
